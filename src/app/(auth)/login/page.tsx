5049c227c2904f1289d7f2c414ea231bc7da5049
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-09-17 23:47:31 +0700

    Add login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
+import LoginForm from "@/app/(auth)/login/login-form";
 import React from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h3 className="text-xl font-semibold text-center">Login</h3>
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
