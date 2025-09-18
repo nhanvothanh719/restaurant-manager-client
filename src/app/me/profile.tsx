e0b3e4508db2d15d6dcb558b97cad9ffebcff4a4
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-09-18 23:28:08 +0700

    Handle error in `Profile` component

--- src/app/me/profile.tsx
+++ src/app/me/profile.tsx
@@ -1,11 +1,16 @@
 "use client";
 import accountApiRequest from "@/apiRequest/account";
+import { handleApiError } from "@/lib/utils";
 import React, { useEffect } from "react";
 
 export default function Profile() {
   useEffect(() => {
     const fetchRequest = async () => {
-      await accountApiRequest.getMeOnClient();
+      try {
+        await accountApiRequest.getMeOnClient();
+      } catch (error) {
+        handleApiError({ error });
+      }
     };
     fetchRequest();
   }, []);
