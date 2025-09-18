8d462fe848df7d3337fc138ab1f753abfcd31dec
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-09-18 22:26:50 +0700

    Add notes-

--- src/apiRequest/account.ts
+++ src/apiRequest/account.ts
@@ -2,12 +2,14 @@ import { AccountResType } from "@/app/schemaValidations/account.schema";
 import http from "@/lib/http";
 
 const accountApiRequest = {
+  // Call API from BE server from server component
   getMe: (sessionToken: string) =>
     http.get<AccountResType>("/account/me", {
       headers: {
         Authorization: `Bearer ${sessionToken}`,
       },
     }),
+  // Call API from BE server from client component (sessionToken is handled by `http`)
   getMeOnClient: () => http.get<AccountResType>("/account/me"),
 };
 
